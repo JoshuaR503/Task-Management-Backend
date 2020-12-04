016916350aa775fc01bf79195216d1292a1a9d96
Joshua Garcia <josshuagarciia000@gmail.com>
2020-12-03 18:21:43 -0800

    Finished adding auth to all endpoints.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -24,8 +24,11 @@ export class TasksController {
     }
  
     @Get('/:id')
-    getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
-        return this.tasksService.getTaskById(id);
+    getTaskById(
+        @Param('id', ParseIntPipe) id: number,
+        @GetUser() user: User,
+    ): Promise<Task> {
+        return this.tasksService.getTaskById(id, user);
     }
 
     @Post()
@@ -38,15 +41,21 @@ export class TasksController {
     }
 
     @Delete('/:id')
-    deleteTaskById(@Param('id') id: number): Promise<void> {
-        return this.tasksService.deleteTask(id);
+    deleteTaskById(
+        @Param('id') id: number,
+        @GetUser() user: User
+
+    ): Promise<void> {
+        return this.tasksService.deleteTask(id, user);
     }
 
-    @Patch('/:id')
+    @Patch('/:id/status')
     updateTaskStatus(
         @Param('id', ParseIntPipe) id: number,
-        @Body('status', TaskStatusValidationPipe) status: TaskStatus
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+        @GetUser() user: User
+
     ): Promise<Task> {
-        return this.tasksService.updateTaskStatus(id, status);
+        return this.tasksService.updateTaskStatus(id, status, user);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -10,20 +10,14 @@ import { TaskStatus } from './tasks.status.enum';
 @Injectable()
 export class TasksService {
 
-    constructor(
-        @InjectRepository(TaskRepository)
-        private taskRepository: TaskRepository
-    ) {}
+    constructor( @InjectRepository(TaskRepository) private taskRepository: TaskRepository) {}
 
-    async getTasks(
-        filterDTO: GetTasksFilterDTO,
-        user: User
-    ): Promise<Task[]> {
+    async getTasks(filterDTO: GetTasksFilterDTO, user: User): Promise<Task[]> {
         return await this.taskRepository.getTasks(filterDTO, user);
     }
 
-    async getTaskById(id: number): Promise<Task> {
-        const found = await this.taskRepository.findOne(id);
+    async getTaskById(id: number, user: User): Promise<Task> {
+        const found = await this.taskRepository.findOne({ where: { id, userId: user.id }});
 
         if (!found) {
             throw new NotFoundException(`Task with id "${id}" not found`);
@@ -32,23 +26,20 @@ export class TasksService {
         return found;
     }
 
-    async createTask(
-        createTaskDTO: CreateTaksDTO,
-        user: User,
-    ): Promise<Task> {
+    async createTask(createTaskDTO: CreateTaksDTO, user: User): Promise<Task> {
         return await this.taskRepository.createTask(createTaskDTO, user);
     }
 
-    async deleteTask(id: number): Promise<void> {
-        const result = await this.taskRepository.delete(id);
+    async deleteTask(id: number, user: User): Promise<void> {
+        const result = await this.taskRepository.delete({id, userId: user.id});
 
         if (result.affected === 0) {
             throw new NotFoundException(`Task with id "${id}" not found`);
         }
     }
 
-    async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
-        const task = await this.getTaskById(id);
+    async updateTaskStatus(id: number, status: TaskStatus, user: User): Promise<Task> {
+        const task = await this.getTaskById(id, user);
 
         task.status = status;
         
